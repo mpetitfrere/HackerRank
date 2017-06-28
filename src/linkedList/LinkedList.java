1516b17135cc676de3b505b1c20feb6e5374553b
mpetitfrere <rentenkz@gmail.com>
2017-06-28 10:14:46 -0400

    Reverse LinkList

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,5 +1,69 @@
 package linkedList;
+class Node {
+    int data;
+    Node next;
+ }
 
 public class LinkedList {
+	
+	
+	static Node initNodes()
+	{
+		//init objects
+		Node a = new Node();
+		Node b = new Node();
+		Node c = new Node();
+		Node d = new Node();
+		Node e = new Node();
+		//init data
+		a.data = 1;
+		b.data = 2;
+		c.data = 3;
+		d.data = 4;
+		e.data = 5;
+		//init link
+		a.next=b;
+		b.next=c;
+		c.next=d;
+		d.next=e;
+		return a;
+		
+	}
+	
+	static void printList(Node node)
+	{
+		while(node!=null)
+		{
+			System.out.print(node.data + "-> ");
+			node=node.next;
+		}
+		System.out.print("null\n");
+
+	}
+	
+	static void ReversePrint(Node head) {
+		Node prev= null;
+		Node loopNode = head;
+		
+		System.out.println("Reverse Link");
+		while(loopNode!=null)
+		{
+			Node node = new Node();
+			node.data=loopNode.data;
+			loopNode= loopNode.next;
+			node.next=prev;
+			prev=node;
+		}
+		printList(prev);
+
+		
+		
+		}
+	public static void main(String[] args) {
+		
+		Node nodeList = initNodes();
+		printList(nodeList);
+		ReversePrint(nodeList);
+	}
 
 }
